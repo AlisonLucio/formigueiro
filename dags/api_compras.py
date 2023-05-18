1d1143b48035b42a2a03aeb3d4c17fb6be2211b9
alison <alisonluciosantos@gmail.com>
2023-05-18 19:00:40 -0300

    dados das APIs chegando até o bigquery

--- dags/api_compras.py
+++ dags/api_compras.py
@@ -3,7 +3,11 @@ from datetime import datetime
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from helpers.apis.dicts.api_compras import CALL_API_GOV, TABLE_NAME_LIST
-# from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+
+from airflow.contrib.operators.gcs_delete_operator import GoogleCloudStorageDeleteOperator
+from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
 
 dag_conf= CALL_API_GOV['DAG_CONFIG']
 
@@ -11,18 +15,29 @@ dag = DAG(
     dag_id=dag_conf['DAG_ID'],
     default_args=dag_conf['DEFAULT_ARGS'],
     schedule_interval=dag_conf['SCHEDULE_INTERVAL'],
-    catchup=dag_conf['CATCHUP']
+    catchup=dag_conf['CATCHUP'],
+    tags=dag_conf['TAGS']
 )
 
 dag_init = DummyOperator(task_id = 'dag_init', dag=dag)
 dag_end = DummyOperator(task_id= 'dag_end', dag=dag)
 
+
 for table_name in TABLE_NAME_LIST:
 
     task_config_list= CALL_API_GOV['TASK_CONFIG']['PIPELINES_TABLES'][table_name]
 
-    extration_api = SimpleHttpOperator(
-        task_id= f"{task_config_list['CLOUD_FUNCTION']['task_id']}_{table_name}",
+    task_delete_file_incoming = GoogleCloudStorageDeleteOperator(
+        task_id=f'delete_file_incoming-{table_name}',
+        bucket_name=task_config_list['DELETE_FILE']['INCOMING']['bucket_name'], 
+        prefix = task_config_list['DELETE_FILE']['INCOMING']['prefix'],
+        gcp_conn_id = task_config_list['DELETE_FILE']['INCOMING']['gcp_conn_id'],  
+        impersonation_chain = task_config_list['DELETE_FILE']['INCOMING']['impersonation_chain'],
+        dag=dag
+    )
+
+    task_extration_api = SimpleHttpOperator(
+        task_id= f"extration-{table_name}",
         method= task_config_list['CLOUD_FUNCTION']['method'],
         http_conn_id= task_config_list['CLOUD_FUNCTION']['http_conn_id'],
         endpoint= task_config_list['CLOUD_FUNCTION']['endpoint'],
@@ -31,19 +46,60 @@ for table_name in TABLE_NAME_LIST:
         dag=dag
     )
 
-    # insert_query_job = BigQueryInsertJobOperator(
-    #     task_id=f"{task_config_list['CLOUD_FUNCTION']['task_id']}_{table_name}",
-    #     configuration={
-    #         "query": {
-    #             "query": INSERT_ROWS_QUERY,
-    #             "useLegacySql": False,
-    #         }
-    #     },
-    #     location=LOCATION,
-    #     deferrable=True,
-    # )
+    task_delete_file_raw = GoogleCloudStorageDeleteOperator(
+        task_id=f'delete_file_raw-{table_name}',
+        bucket_name=task_config_list['DELETE_FILE']['RAW']['bucket_name'], 
+        prefix = task_config_list['DELETE_FILE']['RAW']['prefix'],
+        gcp_conn_id = task_config_list['DELETE_FILE']['RAW']['gcp_conn_id'],
+        impersonation_chain = task_config_list['DELETE_FILE']['RAW']['impersonation_chain'],
+        dag=dag
+    )
 
+    task_incoming_to_raw = GCSToGCSOperator(
+        task_id=f'incoming_to_raw-{table_name}',
+        source_bucket=task_config_list['TRANSFER_STORAGE_DATA']['INCOMING_TO_RAW']['source_bucket'],
+        source_object=task_config_list['TRANSFER_STORAGE_DATA']['INCOMING_TO_RAW']['source_object'],
+        destination_bucket=task_config_list['TRANSFER_STORAGE_DATA']['INCOMING_TO_RAW']['destination_bucket'],
+        destination_object=task_config_list['TRANSFER_STORAGE_DATA']['INCOMING_TO_RAW']['destination_object'],
+        move_object=task_config_list['TRANSFER_STORAGE_DATA']['INCOMING_TO_RAW']['move_object'],
+        dag=dag
+    )
 
-    dag_init >> extration_api >> dag_end
+    task_delete_file_trusted = GoogleCloudStorageDeleteOperator(
+        task_id=f'delete_file_trusted-{table_name}',
+        bucket_name=task_config_list['DELETE_FILE']['TRUSTED']['bucket_name'], 
+        prefix = task_config_list['DELETE_FILE']['TRUSTED']['prefix'],
+        gcp_conn_id = task_config_list['DELETE_FILE']['TRUSTED']['gcp_conn_id'],
+        impersonation_chain = task_config_list['DELETE_FILE']['TRUSTED']['impersonation_chain'],
+        dag=dag
+    )
+
+    task_raw_to_trusted = GCSToGCSOperator(
+        task_id=f'raw_to_trusted-{table_name}',
+        source_bucket=task_config_list['TRANSFER_STORAGE_DATA']['RAW_TO_TRUSTED']['source_bucket'],
+        source_object=task_config_list['TRANSFER_STORAGE_DATA']['RAW_TO_TRUSTED']['source_object'],
+        destination_bucket=task_config_list['TRANSFER_STORAGE_DATA']['RAW_TO_TRUSTED']['destination_bucket'],
+        destination_object=task_config_list['TRANSFER_STORAGE_DATA']['RAW_TO_TRUSTED']['destination_object'],
+        move_object=task_config_list['TRANSFER_STORAGE_DATA']['RAW_TO_TRUSTED']['move_object'],
+        dag=dag
+    )
+
+    task_trusted_to_bigquery = GCSToBigQueryOperator(
+        task_id=f'trusted_to_bigquery-{table_name}',
+        bucket=task_config_list['TRANSFER_STORAGE_DATA']['TRUSTED_TO_BIGQUERY']['bucket'],
+        source_objects=task_config_list['TRANSFER_STORAGE_DATA']['TRUSTED_TO_BIGQUERY']['source_objects'],
+        destination_project_dataset_table=task_config_list['TRANSFER_STORAGE_DATA']['TRUSTED_TO_BIGQUERY']['destination_project_dataset_table'],
+        write_disposition=task_config_list['TRANSFER_STORAGE_DATA']['TRUSTED_TO_BIGQUERY']['write_disposition'],
+        external_table=task_config_list['TRANSFER_STORAGE_DATA']['TRUSTED_TO_BIGQUERY']['external_table'],
+        autodetect=task_config_list['TRANSFER_STORAGE_DATA']['TRUSTED_TO_BIGQUERY']['autodetect'],
+        max_id_key=task_config_list['TRANSFER_STORAGE_DATA']['TRUSTED_TO_BIGQUERY']['max_id_key'],
+        deferrable=task_config_list['TRANSFER_STORAGE_DATA']['TRUSTED_TO_BIGQUERY']['deferrable'],
+        dag=dag
+    )
+
+    dag_init >> task_delete_file_incoming >> task_extration_api >> \
+    task_delete_file_raw >> task_incoming_to_raw >> \
+    task_delete_file_trusted >> task_raw_to_trusted >> task_trusted_to_bigquery >> dag_end
+
 
 
--- dags/helpers/apis/dicts/api_compras.py
+++ dags/helpers/apis/dicts/api_compras.py
@@ -1,26 +1,35 @@
 from datetime import datetime, timedelta
-from helpers.apis.querys.api import incoming_to_raw
+from airflow.models import Variable
+
+PROJECT_ID="video-aulas-ed"
+DATASET_ID_INCOMING="incoming"
+DATASET_ID_RAW="raw"
+DATASET_ID_TRUSTED="trusted"
+LOCATION="us-central1"
+SERVICE_ACCOUNT=Variable.get('SERVICE_ACCOUNT')
+GCP_CONN_ID= 'google_cloud_default'
 
 
-def changePathFile(change_file_type:str, change_table_name:str):
-    return f'layer_incoming/source_type_api/file_type_{change_file_type}/{change_table_name}/'\
+API_SERVICOS_ORGAOS='api_servicos_orgaos'
+API_ORGAOS='api_orgaos'
+API_FORNECEDORES='api_fornecedores'
+
+
+TABLE_NAME_LIST = [
+    API_SERVICOS_ORGAOS,
+    API_ORGAOS,
+    API_FORNECEDORES
+]
+
+def changePathFile(change_layer:str, change_file_type:str, change_table_name:str, change_file_extension:str):
+    return f'layer_{change_layer}/source_type_api/file_type_{change_file_type}/{change_table_name}/'\
             'year={{macros.datetime.strptime(ts_nodash, "%Y%m%dT%H%M%S").strftime("%Y")}}/'\
             'mounth={{macros.datetime.strptime(ts_nodash, "%Y%m%dT%H%M%S").strftime("%m")}}/'\
             'day={{macros.datetime.strptime(ts_nodash, "%Y%m%dT%H%M%S").strftime("%d")}}/'\
             'hour={{macros.datetime.strptime(ts_nodash, "%Y%m%dT%H%M%S").strftime("%H")}}/'\
             'minute={{macros.datetime.strptime(ts_nodash, "%Y%m%dT%H%M%S").strftime("%M")}}/'\
             'prefdate={{macros.datetime.strptime(ts_nodash, "%Y%m%dT%H%M%S").strftime("%Y%m%d")}}/'\
-            f'{change_table_name}.csv'
-
-PROJECT_ID="video-aulas-ed"
-
-
-
-TABLE_NAME_LIST = [
-    'api_servicos_orgaos',
-    'api_orgaos',
-    'api_fornecedores'
-]
+            f'{change_table_name}.{change_file_extension}'
 
 CALL_API_GOV = {
     "DAG_CONFIG":{
@@ -28,15 +37,17 @@ CALL_API_GOV = {
         "PROJECT_ID":PROJECT_ID,
         "DEFAULT_ARGS":{
             'owner':'Alison',
-            'start_date':datetime(2023, 4, 23),
+            'start_date':datetime(2023, 5, 1),
             'retries': 3,
-            'retry_delay': timedelta(minutes=1)
+            'retry_delay': timedelta(seconds=10),
+            'depends_on_past': True
         },
         'SCHEDULE_INTERVAL':'@daily',
-        'CATCHUP':False
+        'CATCHUP':True,
+        'TAGS':['cloud-function', 'bigquery']
     },
     "TASK_CONFIG":{
-        "ZONE":"us-central1",
+        "ZONE":LOCATION,
         "PIPELINES_TABLES":{
             "api_servicos_orgaos":{
                 "CLOUD_FUNCTION":{
@@ -48,21 +59,56 @@ CALL_API_GOV = {
                         'URL_API':'http://compras.dados.gov.br/servicos/v1/servicos.csv',
                         'PROJECT_ID':PROJECT_ID,
                         'BUCKET_NAME':PROJECT_ID,
-                        'PATH_FILE':changePathFile("csv", "api_servicos_orgaos")
+                        'PATH_FILE':changePathFile(change_layer=DATASET_ID_INCOMING, change_file_type="csv", change_table_name=API_SERVICOS_ORGAOS, change_file_extension="csv")
                     },
                     'headers':{"Content-Type": "application/json"}
                 },
-                "BIGQUERY":{
-                    'task_id':'bq_incoming_to_raw',
-                    'location':{
-                        "query":{
-                            "query":incoming_to_raw('api_servicos_orgaos'),
-                            "useLegacySql":False
-                        }
+                'DELETE_FILE':{
+                    'INCOMING':{
+                        'bucket_name':PROJECT_ID,
+                        'prefix':changePathFile(change_layer=DATASET_ID_INCOMING, change_file_type="csv", change_table_name=API_SERVICOS_ORGAOS, change_file_extension="csv"),
+                        'gcp_conn_id':GCP_CONN_ID,
+                        'impersonation_chain':SERVICE_ACCOUNT
                     },
-                    'configuration':'Location',
-                    'deferrable':True
-                }
+                    'RAW':{
+                        'bucket_name':PROJECT_ID,
+                        'prefix':changePathFile(change_layer=DATASET_ID_RAW, change_file_type="csv", change_table_name=API_SERVICOS_ORGAOS, change_file_extension="csv"),
+                        'gcp_conn_id':GCP_CONN_ID,
+                        'impersonation_chain':SERVICE_ACCOUNT
+                    },
+                    'TRUSTED':{
+                        'bucket_name':PROJECT_ID,
+                        'prefix':changePathFile(change_layer=DATASET_ID_TRUSTED, change_file_type="csv", change_table_name=API_SERVICOS_ORGAOS, change_file_extension="csv"),
+                        'gcp_conn_id':GCP_CONN_ID,
+                        'impersonation_chain':SERVICE_ACCOUNT
+                    },
+                },
+                'TRANSFER_STORAGE_DATA':{
+                    'INCOMING_TO_RAW':{
+                        'source_bucket':PROJECT_ID,
+                        'source_object':changePathFile(change_layer=DATASET_ID_INCOMING, change_file_type="csv", change_table_name=API_SERVICOS_ORGAOS, change_file_extension="csv"),
+                        'destination_bucket':PROJECT_ID,
+                        'destination_object':changePathFile(change_layer=DATASET_ID_RAW, change_file_type="csv", change_table_name=API_SERVICOS_ORGAOS, change_file_extension="csv"),
+                        'move_object':True
+                    },
+                     'RAW_TO_TRUSTED':{
+                        'source_bucket':PROJECT_ID,
+                        'source_object':changePathFile(change_layer=DATASET_ID_RAW, change_file_type="csv", change_table_name=API_SERVICOS_ORGAOS, change_file_extension="csv"),
+                        'destination_bucket':PROJECT_ID,
+                        'destination_object':changePathFile(change_layer=DATASET_ID_TRUSTED, change_file_type="csv", change_table_name=API_SERVICOS_ORGAOS, change_file_extension="csv"),
+                        'move_object':False
+                    },
+                    'TRUSTED_TO_BIGQUERY':{
+                        'bucket':PROJECT_ID,
+                        'source_objects':changePathFile(change_layer=DATASET_ID_TRUSTED, change_file_type="csv", change_table_name=API_SERVICOS_ORGAOS, change_file_extension="csv"),
+                        'destination_project_dataset_table': f'trusted.{API_SERVICOS_ORGAOS}',
+                        'write_disposition':'WRITE_APPEND',
+                        'external_table':False,
+                        'autodetect':True,
+                        'max_id_key':None,
+                        'deferrable':False
+                    }                   
+                },               
             },
             'api_orgaos':{
                 "CLOUD_FUNCTION":{
@@ -74,21 +120,56 @@ CALL_API_GOV = {
                         'URL_API':'http://compras.dados.gov.br/licitacoes/v1/orgaos.csv',
                         'PROJECT_ID':PROJECT_ID,
                         'BUCKET_NAME':PROJECT_ID,
-                        'PATH_FILE':changePathFile("csv", "api_orgaos")
+                        'PATH_FILE':changePathFile(change_layer=DATASET_ID_INCOMING, change_file_type="csv", change_table_name=API_ORGAOS, change_file_extension="csv")
                     },
                     'headers':{"Content-Type": "application/json"}
                 },
-                # "BIGQUERY":{
-                #     'task_id':'bq_incoming_to_raw',
-                #     'location':{
-                #         "query":{
-                #             "query":api(''),
-                #             "useLegacySql":False
-                #         }
-                #     },
-                #     'configuration':Location,
-                #     'deferrable':True
-                # }
+                'DELETE_FILE':{
+                    'INCOMING':{
+                        'bucket_name':PROJECT_ID,
+                        'prefix':changePathFile(change_layer=DATASET_ID_INCOMING, change_file_type="csv", change_table_name=API_ORGAOS, change_file_extension="csv"),
+                        'gcp_conn_id':GCP_CONN_ID,
+                        'impersonation_chain':SERVICE_ACCOUNT
+                    },
+                    'RAW':{
+                        'bucket_name':PROJECT_ID,
+                        'prefix':changePathFile(change_layer=DATASET_ID_RAW, change_file_type="csv", change_table_name=API_ORGAOS, change_file_extension="csv"),
+                        'gcp_conn_id':GCP_CONN_ID,
+                        'impersonation_chain':SERVICE_ACCOUNT
+                    },
+                    'TRUSTED':{
+                        'bucket_name':PROJECT_ID,
+                        'prefix':changePathFile(change_layer=DATASET_ID_TRUSTED, change_file_type="csv", change_table_name=API_ORGAOS, change_file_extension="csv"),
+                        'gcp_conn_id':GCP_CONN_ID,
+                        'impersonation_chain':SERVICE_ACCOUNT
+                    },
+                },
+                'TRANSFER_STORAGE_DATA':{
+                    'INCOMING_TO_RAW':{
+                        'source_bucket':PROJECT_ID,
+                        'source_object':changePathFile(change_layer=DATASET_ID_INCOMING, change_file_type="csv", change_table_name=API_ORGAOS, change_file_extension="csv"),
+                        'destination_bucket':PROJECT_ID,
+                        'destination_object':changePathFile(change_layer=DATASET_ID_RAW, change_file_type="csv", change_table_name=API_ORGAOS, change_file_extension="csv"),
+                        'move_object':True
+                    },
+                     'RAW_TO_TRUSTED':{
+                        'source_bucket':PROJECT_ID,
+                        'source_object':changePathFile(change_layer=DATASET_ID_RAW, change_file_type="csv", change_table_name=API_ORGAOS, change_file_extension="csv"),
+                        'destination_bucket':PROJECT_ID,
+                        'destination_object':changePathFile(change_layer=DATASET_ID_TRUSTED, change_file_type="csv", change_table_name=API_ORGAOS, change_file_extension="csv"),
+                        'move_object':False
+                    },
+                    'TRUSTED_TO_BIGQUERY':{
+                        'bucket':PROJECT_ID,
+                        'source_objects':changePathFile(change_layer=DATASET_ID_TRUSTED, change_file_type="csv", change_table_name=API_ORGAOS, change_file_extension="csv"),
+                        'destination_project_dataset_table': f'trusted.{API_ORGAOS}',
+                        'write_disposition':'WRITE_APPEND',
+                        'external_table':False,
+                        'autodetect':True,
+                        'max_id_key':None,
+                        'deferrable':False
+                    }                   
+                },
             },
             'api_fornecedores':{
                 'CLOUD_FUNCTION':{
@@ -100,21 +181,56 @@ CALL_API_GOV = {
                         'URL_API':'http://compras.dados.gov.br/fornecedores/v1/fornecedores.csv',
                         'PROJECT_ID':PROJECT_ID,
                         'BUCKET_NAME':PROJECT_ID,
-                        'PATH_FILE':changePathFile("csv", "api_fornecedores")
+                        'PATH_FILE':changePathFile(change_layer=DATASET_ID_INCOMING, change_file_type="csv", change_table_name=API_FORNECEDORES, change_file_extension="csv")
                     },
                     'headers':{"Content-Type": "application/json"}
                 },
-                # "BIGQUERY":{
-                #     'task_id':'incoming_to_raw',
-                #     'location':{
-                #         "query":{
-                #             "query":file_path,
-                #             "useLegacySql":False
-                #         }
-                #     },
-                #     'configuration':Location,
-                #     'deferrable':True
-                # }
+                'DELETE_FILE':{
+                    'INCOMING':{
+                        'bucket_name':PROJECT_ID,
+                        'prefix':changePathFile(change_layer=DATASET_ID_INCOMING, change_file_type="csv", change_table_name=API_FORNECEDORES, change_file_extension="csv"),
+                        'gcp_conn_id':GCP_CONN_ID,
+                        'impersonation_chain':SERVICE_ACCOUNT
+                    },
+                    'RAW':{
+                        'bucket_name':PROJECT_ID,
+                        'prefix':changePathFile(change_layer=DATASET_ID_RAW, change_file_type="csv", change_table_name=API_FORNECEDORES, change_file_extension="csv"),
+                        'gcp_conn_id':GCP_CONN_ID,
+                        'impersonation_chain':SERVICE_ACCOUNT
+                    },
+                    'TRUSTED':{
+                        'bucket_name':PROJECT_ID,
+                        'prefix':changePathFile(change_layer=DATASET_ID_TRUSTED, change_file_type="csv", change_table_name=API_FORNECEDORES, change_file_extension="csv"),
+                        'gcp_conn_id':GCP_CONN_ID,
+                        'impersonation_chain':SERVICE_ACCOUNT
+                    },
+                },
+                'TRANSFER_STORAGE_DATA':{
+                    'INCOMING_TO_RAW':{
+                        'source_bucket':PROJECT_ID,
+                        'source_object':changePathFile(change_layer=DATASET_ID_INCOMING, change_file_type="csv", change_table_name=API_FORNECEDORES, change_file_extension="csv"),
+                        'destination_bucket':PROJECT_ID,
+                        'destination_object':changePathFile(change_layer=DATASET_ID_RAW, change_file_type="csv", change_table_name=API_FORNECEDORES, change_file_extension="csv"),
+                        'move_object':True
+                    },
+                     'RAW_TO_TRUSTED':{
+                        'source_bucket':PROJECT_ID,
+                        'source_object':changePathFile(change_layer=DATASET_ID_RAW, change_file_type="csv", change_table_name=API_FORNECEDORES, change_file_extension="csv"),
+                        'destination_bucket':PROJECT_ID,
+                        'destination_object':changePathFile(change_layer=DATASET_ID_TRUSTED, change_file_type="csv", change_table_name=API_FORNECEDORES, change_file_extension="csv"),
+                        'move_object':False
+                    },
+                    'TRUSTED_TO_BIGQUERY':{
+                        'bucket':PROJECT_ID,
+                        'source_objects':changePathFile(change_layer=DATASET_ID_TRUSTED, change_file_type="csv", change_table_name=API_FORNECEDORES, change_file_extension="csv"),
+                        'destination_project_dataset_table': f'trusted.{API_FORNECEDORES}',
+                        'write_disposition':'WRITE_APPEND',
+                        'external_table':False,
+                        'autodetect':True,
+                        'max_id_key':None,
+                        'deferrable':False
+                    }     
+                }
             }
         }    
     }
